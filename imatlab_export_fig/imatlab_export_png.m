de3718009a0275326e58536763c4517041c57ff6
imatlab-helper <imatlab-helper@users.noreply.github.com>
2017-03-25 21:40:23 -0700

    remove, don't need

--- imatlab_export_fig/imatlab_export_png.m
+++
@@ -1,7 +0,0 @@
-function imatlab_export_png()
-
-  setenv('IMATLAB_EXPORT_FIG', func2str(@(h) eval([ ...
-      'print(h, [tempname(''.''), ''.png''], ''-dpng'')', ...
-      'close(h);'])));
-
-end
